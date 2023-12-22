00fe680062d195694c1924d7bc61d7d8b8407865
kathelan <mateuszdeptulski@gmail.com>
2023-12-23 00:26:29 +0100

    Initial commit

---
+++ src/main/java/pl/kathelan/budgetifybe/BudgetifyBeApplication.java
@@ -0,0 +1,13 @@
+package pl.kathelan.budgetifybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetifyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetifyBeApplication.class, args);
+    }
+
+}
