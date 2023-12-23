4d920ee1cf797361804de6dddd4ae1730dd53499
kathelan <mateuszdeptulski@gmail.com>
2023-12-23 20:25:34 +0100

    added test controller to check if vps working properly

---
+++ src/main/java/pl/kathelan/budgetifybe/api/TestController.java
@@ -0,0 +1,13 @@
+package pl.kathelan.budgetifybe.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Test successful!";
+    }
+}
