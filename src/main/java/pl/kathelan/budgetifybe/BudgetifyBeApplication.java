21b03af7760f69805e328fed87c82d3bca9e9436
kathelan <mateuszdeptulski@gmail.com>
2023-12-25 16:51:08 +0100

    test

--- src/main/java/pl/kathelan/budgetifybe/BudgetifyBeApplication.java
+++ src/main/java/pl/kathelan/budgetifybe/BudgetifyBeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudgetifyBeApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(BudgetifyBeApplication.class, args);
     }
 
